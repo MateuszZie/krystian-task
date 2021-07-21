5eb063b5bbc3c64ba25a6ea735e3fa1d6f65a74e
Mateusz Ziebura <mateusz_860@op.pl>
2021-07-21 10:18:19 +0200

    Initialize project

---
+++ src/main/java/org/mateuszziebura/krystiantask/KrystianTaskApplication.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.krystiantask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrystianTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrystianTaskApplication.class, args);
+	}
+
+}
